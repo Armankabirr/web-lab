fa04920456f8e43c29aa53fd353efa51428a84e9
Armankabirr <armanutsho12@gmail.com>
2026-06-26 17:30:20 +0600

    done

---
+++ final_q2_251/script.js
@@ -0,0 +1,44 @@
+const goal = 2000;
+let total = 0;
+let entries = 0;
+
+
+
+function calculateCal(){
+    let userInput = document.getElementById("inputCal").value;
+    console.log("num = "+userInput);
+    
+    
+    total = total + Number(userInput);
+
+    console.log(total);
+    
+    let feedback = "";
+    if(total<=800){
+        feedback = "Youre off to a healthy start!";
+    }
+    if(total>=801 && total<=1600){
+        feedback = "Good progress, keep it balanced!";
+    }
+    if(total>=1601 && total<=199){
+        feedback = "Almost at your limit!";
+    }
+    if(total>= goal){
+        feedback = "Goal reached! Stay mindful!";
+    }
+    
+    entries ++;
+
+    if (entries>10 && total<goal){
+        feedback = "Be cautious of frequent snacking!";
+    }
+
+    let show = document.getElementById("total");
+    show.innerHTML = total + " " + feedback;
+
+
+}
+
+
+let btn = document.getElementById("AddBtn");
+btn.addEventListener("click",calculateCal);
